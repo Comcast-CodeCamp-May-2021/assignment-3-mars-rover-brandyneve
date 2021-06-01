69ee3d3174c4fe531cd6ebb9339523b4a29fb520
brandyneve <>
2021-06-01 21:30:26 +0000

    pushing after step 10

--- rover.js
+++ rover.js
@@ -1,5 +1,59 @@
 class Rover {
    // Write code here!
+     constructor(position, mode = "NORMAL", generatorWatts =110){
+     this.position = position;
+     this.mode = mode;
+     this.generatorWatts = generatorWatts
+     }
+     
+     receiveMessage(message){
+       let response = {
+         message: message.name,
+         results: []
+         }
+      
+       //for (let i =0; i < message.commands.length; i++){
+       // response.results.push(message.commands[i]);
+       //}
+       
+         //each command object in command array, push completed: true 
+       //ok, for test 10, i am gonna want to verify that receiveMessage(message) contains the statuscheck command, then receivemessage(message).results should include a roverStatus object with mode, generatorWatts and position
+       //for (let i =0; i < response.results.length; i++){
+       for (let i =0; i < message.commands.length; i++){
+         //if (response.results[i].commandType === "STATUS_CHECK"){
+         if (message.commands[i].commandType === "STATUS_CHECK"){
+          let roverStatusCheck = {
+            completed: true,
+            roverStatus: {
+              mode: this.mode,
+              generatorWatts: this.generatorWatts,
+              position: this.position,
+              }
+            };
+           response.results.push(roverStatusCheck);
+           }
+       
+          /*else if (message.commands[i].commandType === "MODE_CHANGE"){
+              this.mode = message.commands[i].value
+              let roverMode = {
+                completed: true,
+                mode: this.mode
+              }
+              response.results.push(roverMode);
+          /*}else if (//if this.mode=== LOW_POWER && message.commands[i].commandType ==="MOVE"){
+            let roverMove = {
+              completed: false,
+              position= this.position
+            }
+            response.results.push(roverMove)
+          }*/
+        
+      }
+      return response
+     }
 }
+       
+
+
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,74 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  // 7 tests here!"constructor sets position and default values for mode and generatorWatts". Refer to the Rover Class description above for these default values.
+ 
+ it("constructor sets position, default value for mode, and default value for generatorWatts", function() {
+    let rover = new Rover(98382);
+    expect(rover.position).toEqual(98382);
+    expect(rover.mode).toEqual("NORMAL");
+    expect(rover.generatorWatts).toEqual(110);
+  });
+ //test 8 here:"response returned by receiveMessage contains name of message"
+ //a message will be a Message object. returns an object with 2 properties: message will be the name of the original Message object, and results will be an array of results, each element int he array is an object that corresonds to one Command in message.commands   //then finally will update properties of the rover object(see command types table)
+ it("response returned by receiveMessage contains name of message", function(){
+   let rover = new Rover(123456);
+   let commands = [new Command("statusCheck")]
+   let message = new Message("ahhhhhh, Test with two commands",commands);
+   let response = rover.receiveMessage(message);
+   //console.log(response)
+   expect(response.message).toEqual("ahhhhhh, Test with two commands")
+   
+ });
+//test 9 starts here
+it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+   let rover = new Rover(123456);
+   let commands= [new Command("MODE_CHANGE", "LOW_POWER"), new Command("STATUS_CHECK")];
+   let message = new Message("ahhhhhh, Test with two commands", commands);
+   let response = rover.receiveMessage(message);
+   //console.log(message);
+   //console.log(response);
+   expect(response.results.length).toEqual(2)
+   
+ });
+ //test 10 starts here: "responds correctly to status check command" For the STATUS_CHECK command, receiveMessage(message).results includes a roverStatus object describing the current state of the rover object --- mode, generatorWatts, and position. The test should check each of these for accuracy. See the Rover Command Types table for more details.
+it("responds correctly to STATUS_CHECK command", function(){
+   let rover = new Rover(123456);
+   let commands= [ new Command("STATUS_CHECK")];
+   let message = new Message("ahhhhhh, Test with two commands", commands);
+   let response = rover.receiveMessage(message);
+   
+  console.log("test10")
+  console.log(response.results)
+  console.log(response)
+  expect(response.results[0].roverStatus.position).toEqual(rover.position);
+  expect(response.results[0].roverStatus.mode).toEqual(rover.mode);
+  expect(response.results[0].roverStatus.generatorWatts).toEqual(generatorWatts.position);
+ });
+ //test 11 starts here
+ /*it("responds correctly to mode change command", function(){
+   let rover = new Rover(123456);
+   let commands= [ new Command("MODE_CHANGE", "LOW_POWER")];
+   let message = new Message("ahhhhhh, Test with two commands", commands);
+   let response = rover.receiveMessage(message);
+  console.log("test11") 
+  console.log(response.results)
+  console.log(response)
+  expect(response.results[0].roverMode.completed).toEqual(true);
+  expect(response.results[0].roverMode.mode).toEqual(rover.mode);
+  
+ });
+ //test 12 starts here
+ it("responds with false completed value when attempting to move in LOW_POWER mode", function(){
+   let rover = new Rover(1234567);
+   let commands= [ new Command("MOVE", 8675309)];
+   let message = new Message("ahhhhhh, Test with two commands", commands);
+   let response = rover.receiveMessage(message);
+   expect()
 
+ });
+ // test 13 starts here
+ it("responds with position for move command",function(){
+
+ });*/
 });
